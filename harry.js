33853724132264d1542e90e4fd8285362d54cca1
eslamtarek1998 <111464075+eslamtarek1998@users.noreply.github.com>
2022-08-27 19:47:05 +0200

    Add files via upload

---
+++ harry.js
@@ -0,0 +1,17 @@
+
+let gold=document.getElementById("gold")
+let cont=document.querySelector(".cont")
+
+function phones(phone){
+
+    gold.src=phone
+    
+
+}
+
+function colors(color){
+
+    cont.style.background=color;
+
+}
+
